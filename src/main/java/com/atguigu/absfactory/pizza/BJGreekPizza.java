46b28e2003f565cc80046528daee67cb4e6e0f3d
pengh <pengh@shushangyun.com>
2021-02-28 22:20:37 +0800

    尚硅谷设计模式学习---》工厂方法+抽象工厂

---
+++ src/main/java/com/atguigu/absfactory/pizza/BJGreekPizza.java
@@ -0,0 +1,14 @@
+package com.atguigu.absfactory.pizza;
+
+/**
+ * @Author pengheng
+ * @Date 2021/2/28 21:48
+ * @Remark
+ */
+public class BJGreekPizza extends Pizza {
+    @Override
+    public void prepare() {
+        setName("北京希腊披萨");
+        System.out.println(name + "准备中");
+    }
+}
---
+++ src/main/java/com/atguigu/absfactory/pizza/LDCheesePizza.java
@@ -0,0 +1,14 @@
+package com.atguigu.absfactory.pizza;
+
+/**
+ * @Author pengheng
+ * @Date 2021/2/28 21:48
+ * @Remark
+ */
+public class LDCheesePizza extends Pizza {
+    @Override
+    public void prepare() {
+        setName("伦敦奶酪披萨");
+        System.out.println(name + "准备中");
+    }
+}
